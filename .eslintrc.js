70ea345996ad1b2542ab191e82df83072bd6b791
ThanosDi <thanosdi@live.com>
2022-03-28 17:21:56 +0300

    Init commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+/* eslint-disable unicorn/prefer-module */
+module.exports = {
+  // plugins: ['cypress'],
+  // extends: ['plugin:cypress/recommended', 'thechatshop-typescript-prettier/with-react'],
+  // overrides: [
+  // 	{
+  // 		files: ['cypress/**/*'],
+  // 		rules: {
+  // 			'import/no-extraneous-dependencies': [
+  // 				'error',
+  // 				{
+  // 					devDependencies: true,
+  // 				},
+  // 			],
+  // 		},
+  // 	},
+  // ],
+  ignorePatterns: ['build', 'coverage', 'dist'],
+  rules: {
+    // your overrides
+  },
+};
