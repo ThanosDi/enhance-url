10794377462d6e95c197f7dc7e183779ba30ed9f
ThanosDi <thanosdi@live.com>
2022-03-31 13:41:53 +0300

    Add bundling

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import 'rollup';
+import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-js';
+import pkg from './package.json';
+
+const config = {
+    input: 'src/package/enhance-url/index.tsx',
+    output: [
+        {
+            file: './dist/cjs/index.js',
+            format: 'cjs',
+        },
+        {
+            file: './dist/esm/index.js',
+            format: 'es',
+        },
+    ],
+    external: [...Object.keys(pkg.peerDependencies || {})],
+
+    plugins: [
+        nodeResolve(),
+        commonjs(),
+        typescript({
+            typescript: require('typescript'),
+        }),
+    ],
+};
+
+// Minified JS Build
+if (process.env.BUILD === 'minify') {
+    config.plugins.push(uglify.uglify({}, minify));
+}
+
+export default config;
